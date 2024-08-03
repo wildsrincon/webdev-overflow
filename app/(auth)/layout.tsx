97a465c20642d9a9b0b0e3a6d295ebfab909db5d
Wilder Rincon <wildsrincon.developer@gmail.com>
2024-08-03 12:53:11 -0400

    feat: Add auth clerck

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return <main className="flex min-h-screen w-full items-center justify-center">{children}</main>;
+};
+
+export default Layout;
